01619243a448488e297b9150ef768ba7f52e2288
Carl <carl@thecorp.org>
2017-11-15 00:58:09 -0500

    Added Makefile, personalized helloworld.

---
+++ Makefile
@@ -0,0 +1,5 @@
+all:
+	g++ main.cpp -o p4
+
+clean:
+	rm -f main.o p4 core
--- main.cpp
+++ main.cpp
@@ -1,6 +1,19 @@
+/*
+ * Ethics Statement:
+ * Other than anything noted in the disclosures below, the code submitted is of my design and my design alone.
+ * I recieved no help nor used any references, resources, and had no discussion with other classmates.
+ * I understand the University policy concerning ethics and
+ * understand that violating this policy will result in drastic repercussions.
+ *
+ * Disclosures:
+ * No disclosures.
+*/
+
+
 #include <iostream>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+int main()
+{
+    std::cout << "Hello, Carl!" << std::endl;
     return 0;
 }
